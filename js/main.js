b01fdcfe39a60452d62ceaee8e1909de401de3f1
ilya <ilyakhlamov77@gmail.com>
2024-02-03 23:09:22 +0300

    add 04.02.2024

--- js/main.js
+++ js/main.js
@@ -4,3 +4,41 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('menu--open');
 })
+
+
+const swiperOne = new Swiper('.feedback__slider', {
+	
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+})
+
+const swiperTwo = new Swiper('.sertificates__slider', {
+	loop: true,
+	slidesPerView: 3,
+	spaceBetween: 20,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+})
+
+const accordeon = document.querySelector('.accordeon');
+const accordeonTitles = Array.from(accordeon.querySelectorAll('.accordeon__title'));
+
+accordeonTitles.forEach((el, index) => {
+    el.addEventListener('click', function () {
+		const currentText = el.parentElement.querySelector('.accordeon__text')
+
+		currentText.classList.toggle('active');
+    
+        //console.log(currentText);    
+
+	})
+})
